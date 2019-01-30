7206f0d69f3482b2b6cec23379557e3886a9b81f
Miguel Garcia <miguel@acidtango.com>
2019-01-30 18:01:19 +0100

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   rules: {
+    'max-len': [2, 100, { tabWidth: 2, ignoreComments: true }],
+    'comma-dangle': [2, 'always-multiline'],
+    'space-before-function-paren': ['error', 'never'],
+    semi: ['error', 'never'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
+    parser: 'babel-eslint',
+  },
 }
