cda0ff2130fe9f929420acbf667836f5d6a1c6d6
Ruben Moya <ruben@acidtango.com>
2019-02-01 12:37:17 +0100

    📖 Add example of dynamic changing locale

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,20 @@
 <template lang="pug">
-  p {{ $t('message') }}
+
 </template>
+
+<template lang="pug">
+  div.locale-changer
+    select(v-model="$i18n.locale")
+      option(v-for="(lang, i) in langs" v-key="`Lang${i}`" v-value="lang") {{ lang }}
+    p {{ $t('message') }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return { langs: ['es', 'en'] }
+  },
+}
+</script>
